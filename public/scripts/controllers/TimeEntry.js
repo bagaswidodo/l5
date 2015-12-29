95f185d14577a7559f14042f488a004c7e5e57b3
bagaswidodo <bagaswidodo@gmail.com>
2015-12-29 11:53:07 +0700

    add grab user and momentum

--- public/scripts/controllers/TimeEntry.js
+++ public/scripts/controllers/TimeEntry.js
@@ -6,7 +6,7 @@
         .module('timeTracker')
         .controller('TimeEntry', TimeEntry);
 
-        function TimeEntry(time) {
+        function TimeEntry(time, user, $scope) {
 
            // vm is our capture variable
             var vm = this;
@@ -14,10 +14,38 @@
             vm.timeentries = [];
 
             vm.totalTime = {};
+            vm.users = [];
 
             // Initialize the clockIn and clockOut times to the current time.
-            vm.clockIn = new Date();
-            vm.clockOut = new Date();
+            vm.clockIn = moment();
+            vm.clockOut = moment();
+
+            // Grab all the time entries saved in the database
+          getTimeEntries();
+              
+          // Get the users from the database so we can select
+          // who the time entry belongs to
+          getUsers();
+
+            function getUsers() {
+                user.getUsers().then(function(result) {
+                  vm.users = result;
+                }, function(error) {
+                  console.log(error);
+                });
+            }
+          
+          // Fetches the time entries and puts the results
+          // on the vm.timeentries array
+          function getTimeEntries() {
+            time.getTime().then(function(results) {
+              vm.timeentries = results;
+                updateTotalTime(vm.timeentries);
+                console.log(vm.timeentries);
+              }, function(error) {
+                console.log(error);
+              });
+            }         
 
             // Fetches the time entries from the static JSON file
             // and puts the results on the vm.timeentries array
