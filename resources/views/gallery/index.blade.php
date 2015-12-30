149a8b6f8435c974536ca8c9b5fa0a58b81a0e81
bagaswidodo <bagaswidodo@gmail.com>
2015-12-30 09:45:49 +0700

    gallery view starter

---
+++ resources/views/gallery/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html ng-app="myApp">
+<head>
+	<title>Laravel 5 Gallery SPA</title>
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/dist/css/bootstrap.min.css">
+	<script>
+	 var baseUrl = "{{ url('/') }}/";
+	  var csrfToken = "{{ csrf_token() }}";
+	</script>
+</head>
+<body>
+	<div class="container" ng-controller="globalController">
+		<div ng-view></div>
+	</div>
+
+	<script src="vendor/jquery/dist/jquery.min.js"></script>
+	<script src="vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="vendor/angular/angular.min.js"></script>
+	<script src="vendor/angular-route/angular-route.min.js"></script>
+	<script src="vendor/angular-cookies/angular-cookies.min.js"></script>
+	<script src="elixir/js/app.js"></script>
+	<script src="elixir/js/models.js"></script>
+	<script src="elixir/js/controller.js"></script>
+
+
+</body>
+</html>
