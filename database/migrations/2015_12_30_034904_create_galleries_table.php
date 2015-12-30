de2b6dc5ec5c204befba93c02c28060f03082fd1
bagaswidodo <bagaswidodo@gmail.com>
2015-12-30 11:10:12 +0700

    galleri database schema

---
+++ database/migrations/2015_12_30_034904_create_galleries_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('user_id')->unsigned()->index();
+            $table->timestamps();
+
+            //set integrity
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('galleries');
+    }
+}
